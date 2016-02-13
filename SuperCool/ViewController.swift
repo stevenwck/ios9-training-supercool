f4f4f4c16b1da500d70b9eb31fb749725e942aad
Steven Wong <steven@Stevens-iMac.local>
2016-02-14 10:32:36 +1100

    Initial checkin of all SuperCool assets

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Steven Wong on 13/02/2016.
+//  Copyright © 2016 Steven CK Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBG: UIImageView!
+    @IBOutlet weak var uncoolBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        coolBG.hidden = false
+        coolLogo.hidden = false
+        uncoolBtn.hidden = true
+    }
+
+}
+
